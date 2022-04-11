8cfa41a7874633084182e166a0b760873e7c3dbe
mdsagar333 <md_sagar@yahoo.com>
2022-04-12 00:25:05 +0600

    server.js file changed

--- server.js
+++ server.js
@@ -1,25 +1,4 @@
-const { MongoClient } = require("mongodb");
 const app = require("./index");
-
-// const uri =
-//   "mongodb+srv://ayan:KZuASguNQ9rCZfh2@cluster0.tcz9h.mongodb.net/akacoin?retryWrites=true&w=majority";
-
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// client
-//   .connect()
-//   .then(() => {
-//     console.log("DB connectted successfully!");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// const database = client.db("akacoin");
-// module.exports.Banner = database.collection("heroBanner");
 
 const port = process.env.PORT || 5000;
 
